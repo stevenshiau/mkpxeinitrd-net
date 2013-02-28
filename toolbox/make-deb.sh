c655cfd89d72dd90c2de7ee36cb1d8337873b820
Steven Shiau <steven@nchc.org.tw>
2013-02-28 13:56:33 +0800

    Instead of using "debuild -j8" in make-deb.sh, put CPU option in Makefile.

--- toolbox/make-deb.sh
+++ toolbox/make-deb.sh
@@ -39,5 +39,5 @@ tar -xvjf $TARBALL -C debforge/
 #[ -e "$busybox_pkg" ] && cp $busybox_pkg debforge/$PKG-$VER/initrd/
 cp -a debian debforge/$PKG-$VER/
 cd debforge/$PKG-$VER
-debuild -j8
+debuild
 rm -f $TARBALL_ORIG
